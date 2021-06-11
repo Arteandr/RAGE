eb92896ff4ecf462a145586092b79a46d0a546c0
Arteandr <artedamatik@gmail.com>
2021-06-11 22:46:14 +0300

    [initial]

---
+++ dotnet/resources/gamemode/Server/Core/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using GTANetworkAPI;
+
+namespace Gamemode.Server.Core
+{
+    class Program : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStart()
+        {
+            NAPI.Util.ConsoleOutput("[SUCCESS] Server started");
+        }
+    }
+}
